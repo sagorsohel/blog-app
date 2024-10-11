8def5daf9c2c4ed8ac5213c310861a52c45d4cdd
sagorsohel <sohel19sagor@gmail.com>
2024-10-11 09:08:17 +0600

    add comment model

---
+++ models/Comment.js
@@ -0,0 +1,44 @@
+// user, post , body, replies
+
+
+const {Schema,model}=require('mongoose')
+
+const commentSchema= new Schema({
+    post:{
+        type:Schema.Types.ObjectId,
+        ref:'Post',
+        required:true
+    },
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    body:{
+        type:String,
+        required:true,
+        trim:true
+
+    },
+    replies:[{
+        body:{
+            type:String,
+            required:true,
+           
+        },
+        user:{
+            type:Schema.Types.ObjectId,
+            ref:'User',
+            required:true
+        },
+        createdAt:{
+            type:Date,
+            default:new Date()
+        }
+
+    }]
+},{
+    timestamps:true
+})
+const Comment =model('Comment',commentSchema)
+module.exports = Comment
