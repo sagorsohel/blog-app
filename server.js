123815b78856d13edfd5b79043586030e95bd3ad
sagorsohel <sohel19sagor@gmail.com>
2024-11-12 00:37:18 +0600

    fkjk

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-const port = 7000;
+const port = 8000;
 const authRoute = require('./routes/authRoute')
 app.use(express.json());
 
